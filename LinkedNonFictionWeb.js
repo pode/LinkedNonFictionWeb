5f0b48d43a8d7f586d48bc98fc3c70640123c0c2
Magnus Enger <magnus@torfeus.deich.folkebibl.no>
2010-11-24 11:43:38 +0100

    Issue 3. Show Dewey-numbers in grey.

--- LinkedNonFictionWeb.js
+++ LinkedNonFictionWeb.js
@@ -25,7 +25,7 @@ function populateLanguageChooser() {
 function showTopConcepts() {
 
 	// Clear the list of concepts
-	$('#concepts').empty();
+	$('.concepts').empty();
 	
 	// Show "top concepts"
 			 var top_sparql = 'PREFIX skos: <http://www.w3.org/2004/02/skos/core#> \n';
@@ -48,12 +48,56 @@ function showTopConcepts() {
 				// http://dewey.info/class/0/2009/08/about.en and http://dewey.info/class/000/2009/07/about.en
 				// Here we filter out the ones that contain a 3 digit dewey number, based on the length of the concept URI
 				if (item.concept.value.length == 42) {
-					$('#concepts').append('<li class="topconcept">' + item.notation.value + ' ' + item.label.value + '</li>');
+					$('#concepts1').append('<li class="concept1" onClick="showNarrower(\'' + item.concept.value + '\', \'concept2\');"><span class="notation">' + item.notation.value + '</span> ' + item.label.value + '</li>');
 				}
 			});
 		} else {
 			alert('Something went wrong...');
 		}
 	});
+	
+}
+
+function showNarrower(uri, level) {
+	
+	// Clear the list of concepts
+	if (level == 'concept2') {
+		$('#concepts2').empty();
+	} 
+	$('#concepts3').empty();
+	
+	// Show "narrower concepts" as seen from the given URI
+			 var top_sparql = 'PREFIX skos: <http://www.w3.org/2004/02/skos/core#> \n';
+	top_sparql = top_sparql + 'PREFIX dct: <http://purl.org/dc/terms/> \n';
+	top_sparql = top_sparql + 'SELECT DISTINCT ?narrower ?notation ?label WHERE { \n';
+	top_sparql = top_sparql + '<' + uri + '> skos:narrower ?narrower . \n';
+	top_sparql = top_sparql + '?narrower skos:notation ?notation . \n';
+	top_sparql = top_sparql + '?narrower skos:prefLabel ?label . \n';
+	top_sparql = top_sparql + '?narrower dct:language "' + $("#lang_select option:selected").val() + '" . \n';
+	top_sparql = top_sparql + '}';
+	
+	var top_url = 'http://dewey.info/sparql.php?query=' + escape(top_sparql) + '&jsonp=?';
+	var params = { 'output': 'json' };
+
+	$.getJSON(top_url, params, function(json, status) {
+		if (json.results.bindings){
+			$.each(json.results.bindings, function(i, n) {
+				var item = json.results.bindings[i];
+				if (level == 'concept2') {
+					$('#concepts2').append('<li class="concept2" onClick="showNarrower(\'' + item.narrower.value + '\', \'concept3\');"><span class="notation">' + item.notation.value + '</span> ' + item.label.value + '</li>');
+				} else {
+					$('#concepts3').append('<li class="concept3" onClick="showResults(\'' + item.narrower.value + '\');"><span class="notation">' + item.notation.value + '</span> ' + item.label.value + '</li>');	
+				}
+			});
+		} else {
+			alert('Something went wrong...');
+		}
+	});
+	
+}
+
+function showResults(uri) {
+
+	alert("Treff for " + uri);
 	
 }
