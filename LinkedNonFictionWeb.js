d294129ea0b514a4ab406a33cc1519217f030040
Magnus Enger <magnus@torfeus.deich.folkebibl.no>
2010-11-25 17:19:59 +0100

    Isse 2. Start to implement hitlist.

--- LinkedNonFictionWeb.js
+++ LinkedNonFictionWeb.js
@@ -27,7 +27,7 @@ function showTopConcepts() {
 	// Clear the list of concepts
 	$('.concepts').empty();
 	// Clear search results
-	$('#searchresults').empty();
+	$('.resultrow').remove();
 	
 	// Show "top concepts"
 			 var top_sparql = 'PREFIX skos: <http://www.w3.org/2004/02/skos/core#> \n';
@@ -124,6 +124,53 @@ function showResults(uri, id) {
 	$('#concepts3 .label').css('background-color', 'white');
 	// Highlight the chosen concept
 	$('#' + id + ' .label').css('background-color', 'silver');
-	$('#searchresults').empty().append("Treff for " + uri);
+	
+	// Make sure no results are displayed
+	$('.resultrow').remove();
+	
+	// Debug
+	// $('#debug').empty().append('DEBUG Treff for ' + uri);
+	
+	// Turn the uri into something we can use
+	var class = uri.substring(24, 27);
+	
+	// Get results
+				var search_sparql = 'PREFIX pode: <http://www.bibpode.no/vocabulary#> ';
+	search_sparql = search_sparql + 'PREFIX dct: <http://purl.org/dc/terms/> ';
+	search_sparql = search_sparql + 'PREFIX foaf: <http://xmlns.com/foaf/0.1/> ';
+	search_sparql = search_sparql + 'SELECT DISTINCT ?post ?name ?title ?format ?issued ?langlabel WHERE { ';
+	search_sparql = search_sparql + '?post dct:source pode:dfb_fagposter ; ';
+	search_sparql = search_sparql + 'pode:ddkThird <http://www.bibpode.no/instance/DDK_' + class + '> ; ';
+	search_sparql = search_sparql + 'dct:title ?title ; ';
+	search_sparql = search_sparql + 'dct:format ?format ; ';
+	search_sparql = search_sparql + 'dct:issued ?issued ; ';
+	search_sparql = search_sparql + 'dct:language ?language ; ';
+	search_sparql = search_sparql + 'dct:creator ?creator . ';
+	search_sparql = search_sparql + '?creator foaf:name ?name . '; 
+	search_sparql = search_sparql + '?language rdfs:label ?langlabel . '; 
+	search_sparql = search_sparql + 'FILTER langMatches( datatype(?langlabel), "xsd:stringno" ) ';
+	search_sparql = search_sparql + '} ORDER BY DESC(?issued) LIMIT 10 ';
+	
+	var search_url = 'http://bibpode.no/rdfstore/endpoint.php?query=' + escape(search_sparql) + '&output=json&jsonp=?';
+	var params = { 'output': 'json' };
+	
+	// $('#debug').append('<br /><br />' + search_url);
+
+	$.getJSON(search_url, params, function(json, status) {
+		if (json.results.bindings){
+			// Make sure the results-table is displayed
+			// $('#searchresults').show();
+			var c = 1;
+			$.each(json.results.bindings, function(i, n) {
+				var item = json.results.bindings[i];
+				$('#searchresults').append('<tr class="resultrow"><td>' + c + '</td><td>' + item.name.value + '</td><td>' + item.title.value + '</td><td>' + item.format.value + '</td><td>' + item.issued.value + '</td><td>' + item.langlabel.value + '</td></tr>');
+				c = c + 1;
+			});
+			$('#searchresults').show();
+		} else {
+			alert('Something went wrong...');
+		}
+	});
+
 	
 }
